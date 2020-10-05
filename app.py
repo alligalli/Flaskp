c66181c2dbe334db71b21d7c3045ae509fec1354
alligalli <34606119+alligalli@users.noreply.github.com>
2020-10-05 20:01:03 +0200

    added login form & logic

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_migrate import Migrate
-from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
-
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
 load_dotenv()
@@ -86,6 +88,12 @@ class CustomAdminIndexView(AdminIndexView):
     def inaccessible_callback(self, username, **kwargs):
         return redirect(url_for('login'))
 
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Login')
+
 admin = Admin(app, name='Flasky ADM', template_mode='bootstrap4', index_view=CustomAdminIndexView())
 admin.add_view(CustomModelView(User, db.session))
 admin.add_view(CustomModelView(Role, db.session))
@@ -102,16 +110,26 @@ def about():
 def blog():
     return render_template('blog.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    user = User.query.get(1)
-    login_user(user)
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('index')
+            return redirect(next)
+        flash('Invalid username or password.')
+    return render_template('login.html', form=form)
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
-    return render_template('logout.html')
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
 
 @app.errorhandler(403)
 def forbidden(e):
